9de2886bc60c328815c6e75a3733df4b4af8ea76
“Jun <2101622@sit.singaporetech.edu.sg>
2023-03-21 23:17:47 +0800

    GUI

--- main.py
+++ main.py
@@ -4,70 +4,40 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.hashes import SHA256
 from cryptography.hazmat.backends import default_backend
 from base64 import urlsafe_b64encode
+import tkinter as tk
+from tkinter import filedialog, messagebox
 
 # Define constants
 ITERATIONS = 100000
 LENGTH = 32
 
-# Get passphrase from user
-passphrase = input("Enter passphrase: ")
+class App:
+    def __init__(self, master):
+        self.master = master
+        master.title("File Encryption/Decryption")
 
-# Use passphrase to generate key
-salt = os.urandom(16)
-kdf = PBKDF2HMAC(
-    algorithm=SHA256(),
-    length=LENGTH,
-    salt=salt,
-    iterations=ITERATIONS,
-    backend=default_backend()
-)
-key = kdf.derive(passphrase.encode())
-fernet_key = urlsafe_b64encode(key)
+        self.label_passphrase = tk.Label(master, text="Passphrase:")
+        self.label_passphrase.grid(row=0, column=0, padx=5, pady=5)
 
-# Check if key is correct
-try:
-    fernet = Fernet(fernet_key)
-    fernet.decrypt(fernet.encrypt(b"test"))
-except:
-    print("Incorrect passphrase. Exiting...")
-    exit()
+        self.entry_passphrase = tk.Entry(master, show="*")
+        self.entry_passphrase.grid(row=0, column=1, padx=5, pady=5)
 
-# Get user's choice to encrypt or decrypt
-choice = input("Enter 'e' to encrypt or 'd' to decrypt: ")
+        self.var = tk.StringVar(value="encrypt")
+        self.radio_encrypt = tk.Radiobutton(master, text="Encrypt", variable=self.var, value="encrypt")
+        self.radio_encrypt.grid(row=1, column=0, padx=5, pady=5)
 
-if choice == 'e':
-    # Get file name from user
-    filename = input("Enter file name to encrypt: ")
+        self.radio_decrypt = tk.Radiobutton(master, text="Decrypt", variable=self.var, value="decrypt")
+        self.radio_decrypt.grid(row=1, column=1, padx=5, pady=5)
 
-    # Encrypt file
-    try:
-        with open(filename, 'rb') as f:
-            data = f.read()
+        self.button_execute = tk.Button(master, text="Execute", command=self.execute)
+        self.button_execute.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
 
-        encrypted_data = fernet.encrypt(data)
+    def encrypt(self):
+        # Get passphrase from user
+        passphrase = self.entry_passphrase.get()
 
-        with open(filename, 'wb') as f:
-            f.write(encrypted_data)
-
-        print("File encrypted successfully.")
-
-        # Write salt to file
-        with open('salt.txt', 'wb') as f:
-            f.write(salt)
-
-    except FileNotFoundError:
-        print("File not found. Exiting...")
-        exit()
-
-elif choice == 'd':
-    # Get file name from user
-    filename = input("Enter file name to decrypt: ")
-
-    # Decrypt file using key
-    try:
-        with open('salt.txt', 'rb') as f:
-            salt = f.read()
-
+        # Use passphrase to generate key
+        salt = os.urandom(16)
         kdf = PBKDF2HMAC(
             algorithm=SHA256(),
             length=LENGTH,
@@ -77,26 +47,104 @@ elif choice == 'd':
         )
         key = kdf.derive(passphrase.encode())
         fernet_key = urlsafe_b64encode(key)
-        fernet = Fernet(fernet_key)
 
-        with open(filename, 'rb') as f:
-            encrypted_data = f.read()
+        # Check if key is correct
+        try:
+            fernet = Fernet(fernet_key)
+            fernet.decrypt(fernet.encrypt(b"test"))
+        except:
+            messagebox.showerror("Error", "Incorrect passphrase.")
+            return
 
-        decrypted_data = fernet.decrypt(encrypted_data)
 
-        with open(filename, 'wb') as f:
-            f.write(decrypted_data)
+        # Get file to encrypt/decrypt from user
+        if self.var.get() == "encrypt":
+            filename = filedialog.askopenfilename()
+            if not filename:
+                return
 
-        print("File decrypted successfully.")
+        # Encrypt file
+        try:
+            with open(filename, 'rb') as f:
+                data = f.read()
 
-    except FileNotFoundError:
-        print("File not found. Exiting...")
-        exit()
+            encrypted_data = fernet.encrypt(data)
 
-    except:
-        print("Incorrect passphrase, salt, or key. Exiting...")
-        exit()
+            with open(filename, 'wb') as f:
+                f.write(encrypted_data)
 
-else:
-    print("Invalid choice. Exiting...")
-    exit()
+            # Write salt to file
+            with open('salt.txt', 'wb') as f:
+                f.write(salt)
+
+            messagebox.showinfo("Success", "File encrypted successfully.")
+
+        except FileNotFoundError:
+            messagebox.showerror("Error", "File not found.")
+        except Exception as e:
+            messagebox.showerror("Error", "Unable to encrypt file.")
+
+    def decrypt(self):
+        # Get passphrase from user
+        passphrase = self.entry_passphrase.get()
+
+        # Use passphrase to generate key
+        try:
+            with open('salt.txt', 'rb') as f:
+                salt = f.read()
+
+            kdf = PBKDF2HMAC(
+                algorithm=SHA256(),
+                length=LENGTH,
+                salt=salt,
+                iterations=ITERATIONS,
+                backend=default_backend()
+            )
+            key = kdf.derive(passphrase.encode())
+            fernet_key = urlsafe_b64encode(key)
+
+            # Check if key is correct
+            fernet = Fernet(fernet_key)
+            fernet.decrypt(fernet.encrypt(b"test"))
+
+        except FileNotFoundError:
+            messagebox.showerror("Error", "Salt file not found.")
+            return
+        except:
+            messagebox.showerror("Error", "Incorrect passphrase.")
+            return
+
+        # Get file to encrypt/decrypt from user
+        if self.var.get() == "decrypt":
+            filename = filedialog.askopenfilename()
+            if not filename:
+                return
+
+        # Decrypt file
+        try:
+            with open(filename, 'rb') as f:
+                data = f.read()
+
+            decrypted_data = fernet.decrypt(data)
+
+            with open(filename, 'wb') as f:
+                f.write(decrypted_data)
+
+            messagebox.showinfo("Success", "File decrypted successfully.")
+
+        except FileNotFoundError:
+            messagebox.showerror("Error", "File not found.")
+        except:
+            messagebox.showerror("Error", "Unable to decrypt file.")
+
+    def execute(self):
+        choice = self.var.get()
+        if choice == "encrypt":
+            self.encrypt()
+        elif choice == "decrypt":
+            self.decrypt()
+
+
+root = tk.Tk()
+app = App(root)
+root.mainloop()
