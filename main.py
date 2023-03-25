1a5233a5be257e7222c4da09025646dc8692d230
Mosipa <jiaric@hotmail.com>
2023-03-25 15:38:12 +0800

    Added 10 tries deletion

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 import hashlib
 import datetime
+import pickle
 
 # Define constants
 ITERATIONS = 16384
 LENGTH = 32
 SECKEY = "2205"
+POOKI = "res\\pooki.pk1"
 
 class App:
     def __init__(self, master):
         self.master = master
         master.title("File Encryption/Decryption")
+        self.failed_attempts = self.load_failed_attempts()
 
         self.label_passphrase = tk.Label(master, text="Passphrase:")
         self.label_passphrase.grid(row=0, column=0, padx=5, pady=5)
@@ -26,6 +29,16 @@ class App:
 
         self.button_execute = tk.Button(master, text="Execute", command=self.execute)
         self.button_execute.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+
+    def load_failed_attempts(self):
+        if os.path.exists(POOKI):
+            with open(POOKI, "rb") as f:
+                return pickle.load(f)
+        return {}
+
+    def save_failed_attempts(self):
+        with open(POOKI, "wb") as f:
+            pickle.dump(self.failed_attempts, f)
 
     def encrypt(self, filename):
         # Get passphrase from user
@@ -145,22 +158,39 @@ class App:
 
             messagebox.showinfo("Success", "File decrypted successfully.")
             os.remove(filepath)
+            if filename in self.failed_attempts:
+                del self.failed_attempts[filename]
+                self.save_failed_attempts()
 
         except FileNotFoundError:
             messagebox.showerror("Error", "File not found.")
         except Exception as e:
-            messagebox.showerror("Error", "Unable to decrypt file. " + str(e))
+            if filename not in self.failed_attempts:
+                self.failed_attempts[filename] = 0
+
+            self.failed_attempts[filename] += 1
+            self.save_failed_attempts()
+
+            if self.failed_attempts[filename] >= 10:
+                os.remove(filename)
+                del self.failed_attempts[filename]
+                os.remove(filepath)
+                self.save_failed_attempts()
+                messagebox.showerror("Error", "Unable to decrypt file. File deleted after 10 failed attempts.")
+            else:
+                messagebox.showerror("Error", "Unable to decrypt file. " + str(e))
 
     def execute(self):
         try:
-            # Open folder to select file
-            filename = filedialog.askopenfilename()
-            if not filename:
-                return
-
+            # Check if passphrase is not empty
             passphrase = self.entry_passphrase.get()
             if not passphrase:
                 messagebox.showerror("Error", "Please enter a passphrase.")
+                return
+            
+            # Open folder to select file
+            filename = filedialog.askopenfilename()
+            if not filename:
                 return
 
             sha256_hash = hashlib.sha256()
